1c20e54eff1759fce0543dfc30b7443f1efbecb7
haikalrozaidi <binrozaidi.1@osu.edu>
2023-12-06 09:45:53 -0500

    MiscGame1Changes

--- sprint2/Game1.cs
+++ sprint2/Game1.cs
@@ -267,7 +267,7 @@ namespace sprint2
                     collision.HandlePlayerBlockCollision(player, blocks);
                     collision.HandlePlayerEnemyCollision(player, NPCList);
                     collision.HandlePlayerEnemyCollision(player, groundHit);
-                    collision.HandleEnemyEnemyCollision(NPCList);
+                    //collision.HandleEnemyEnemyCollision(NPCList);
                     collision.HandleEnemyBlockCollision(NPCList, blocks);
                     collision.HandleEnemyProjectileCollision(NPCList, playerProjectiles);
                     collision.HandleEnemyWallCollision(NPCList, wallHitboxes);
